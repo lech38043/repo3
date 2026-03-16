3d8e0f1b820f369a87d942e9847060079224ffa6
lech38043 <ppx38043@student.poznan.merito.pl>
2026-03-16 19:27:38 +0100

    github actions test10

--- database/001_create_table.sql
+++ database/001_create_table.sql
@@ -1,17 +1,19 @@
-DROP TABLE IF EXISTS clean_data;
+IF OBJECT_ID('dbo.clean_data', 'U') IS NOT NULL
+    DROP TABLE dbo.clean_data;
+GO
 CREATE TABLE clean_data(
-    "brand" NVARCHAR(50),
-    "model" NVARCHAR(50),
-    "price_usd" BIGINT NULL,
-    "ram_gb" BIGINT NULL,
-    "storage_gb" BIGINT NULL,
-    "camera_mp" BIGINT NULL,
-    "battery_mah" BIGINT NULL,
-    "display_size_inch" FLOAT,
-    "charging_watt" BIGINT NULL,
-    "5g_support" BIT,
-    "os" NVARCHAR(50),
-    "processor" NVARCHAR(50),
-    "rating" FLOAT,
-    "release_month" NVARCHAR(50),
-    "year" BIGINT NULL);
+    [brand] NVARCHAR(50),
+    [model] NVARCHAR(50),
+    [price_usd] BIGINT NULL,
+    [ram_gb] BIGINT NULL,
+    [storage_gb] BIGINT NULL,
+    [camera_mp] BIGINT NULL,
+    [battery_mah] BIGINT NULL,
+    [display_size_inch] FLOAT,
+    [charging_watt] BIGINT NULL,
+    [5g_support] BIT,
+    [os] NVARCHAR(50),
+    [processor] NVARCHAR(50),
+    [rating] FLOAT,
+    [release_month] NVARCHAR(50),
+    [year] BIGINT NULL);
